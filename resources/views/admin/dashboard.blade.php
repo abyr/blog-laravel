08e5eba9f73888e4827a433cc03ecc1ce4de98cf
Aleksandr Denisenko <abyrcheg@gmail.com>
2018-03-26 22:36:39 +0300

    add reate category link, ignore nb

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -35,7 +35,8 @@
 
         <div class="row">
             <div class="col-sm-6">
-                <a class="btn btn-block btn-default" href="#">Create category</a>
+                <a class="btn btn-block btn-default"
+                    href="{{route('admin.category.create')}}">Create category</a>
                 <a href="#" class="list-group-item">
                     <h4 class="list-group-item-heading">Category 1</h4>
                     <p class="list-group-item-text">
