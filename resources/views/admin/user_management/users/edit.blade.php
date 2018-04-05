4253c6091ded68b4d4cfc46e6f6d77a9cb0f0d1a
Aleksandr Denisenko <abyrcheg@gmail.com>
2018-04-05 13:31:55 +0300

    edit user

---
+++ resources/views/admin/user_management/users/edit.blade.php
@@ -0,0 +1,24 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+
+<div class="container">
+
+    @component('admin.components.breadcrumb')
+        @slot('title') Edit user @endslot
+        @slot('parent') Main @endslot
+        @slot('active') user @endslot
+    @endcomponent
+
+<hr>
+
+<form class="form-horizontal" action="{{route('admin.user_management.user.update', $user )}}" method="post">
+    {{ method_field('PUT')}}
+    {{ csrf_field() }}
+
+    @include('admin.user_management.users.partials.form')
+</form>
+
+</div>
+
+@endsection
